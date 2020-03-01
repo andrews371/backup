ee246b2609011c334870df8fd31020254f5ca3b6
andrews371 <aoliveira371@gmail.com>
2020-02-29 23:37:00 -0300

    backups

--- 2019.2/C.G./leitor_blender.c
+++ 2019.2/C.G./leitor_blender.c
@@ -1,7 +1,10 @@
+// Andre Oliveira de Sousa - 11325684
+
 #include<stdio.h>    
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
+#include<GL/glut.h>
 
 // estrutura do nó de faces da lista
 typedef struct lados
@@ -18,6 +21,205 @@ typedef struct vertices
 	struct vertices *prox;
 
 }Vertices;
+
+int tam_faces = 0, tam_vertices = 0, tam_vert_por_face = 0;
+Lados *lista_faces = NULL;
+Vertices *lista_vertices = NULL;
+
+
+// protótipos
+void desenha_obj();
+void init();
+Lados* inserir_fim_face(Lados *lista, int dado);
+Vertices* inserir_fim_vertice(Vertices *lista, float x, float y, float z);
+Lados* busca_face(int f, Lados *lista, int *tam_faces);
+Vertices* busca_vertice(int v, Vertices *lista, int *tam_vertices);
+
+// função principal
+int main(int argc, char** argv)
+{
+	// lendo arquivo
+	FILE *arq;
+	char *ler;
+	char linha[100];
+	char str[10000] = "";
+	int i, num;
+
+	// lendo arquivo todo
+	arq = fopen("cubo.x3d", "r");
+	while(!feof(arq)){
+		ler = fgets(linha, 100, arq);
+		if (ler){
+			strcat(str, linha);
+		}
+		i++;
+	}
+	fclose(arq);
+	printf("\n");
+
+	// Variáveis usadas para ler os vértices e faces desejadas
+	char *separar;
+	char separador[] = " \n";
+	int flag = 0;
+	int flag_coord = 1;
+	char face[4];
+	char vert[10];
+	float dado;
+	float coord[3];
+	bool flag_vert_face = false;
+
+	// Lendo só os vértices e faces desejadas
+	separar = strtok(str, separador);
+	while(separar != NULL){
+
+		if (separar[0] == 'c' && separar[1] == 'o' && separar[2] == 'o' && separar[3] == 'r' && separar[4] == 'd' &&\
+			separar[5] == 'I' && separar[6] == 'n' && separar[7] == 'd' && separar[8] == 'e' && separar[9] == 'x') {
+
+				flag = 1;
+				for (int i = 0; i < strlen(separar) - 1; i++){
+					if (separar[i] == '"'){
+						strcat(face, &separar[i + 1]);						
+						dado = atoi(face);				
+						lista_faces = inserir_fim_face(lista_faces, dado);
+						tam_vert_por_face++;
+					}
+				}
+		}
+
+		else if (separar[0] == 'p' && separar[1] == 'o' && separar[2] == 'i' && separar[3] == 'n' && separar[4] == 't' &&\
+			separar[5] == '=' && separar[6] == '"') {
+
+				flag = 2;
+				for (int i = 0; i < strlen(separar) - 1; i++){
+					if (separar[i] == '"'){
+						strcat(vert, &separar[i + 1]);						
+						dado = atof(vert);
+
+						// setando o x do vértice
+						coord[0] = dado;
+						flag_coord++;
+					}
+				}
+		}
+
+		else if (strcmp(separar, "\"") == 0){
+			flag = 0;
+		}
+
+		else if (flag == 1){
+			dado = atoi(separar);
+			if (dado != -1){
+				lista_faces = inserir_fim_face(lista_faces, dado);
+			}		
+			
+			if (dado == -1){
+				tam_faces++;
+				flag_vert_face = true;
+			}
+			if (flag_vert_face == false){
+				tam_vert_por_face++;
+			}
+		}
+
+		else if (flag == 2){
+			dado = atof(separar);	
+
+			if (flag_coord == 1){
+				// setando o x do vértice
+				coord[0] = dado;
+				flag_coord++;
+			}
+
+			else if (flag_coord == 2){
+				// setando o y do vértice
+				coord[1] = dado;
+				flag_coord++;
+			}
+
+			else {
+				// setando o z do vértice
+				coord[2] = dado;
+				flag_coord = 1;
+				lista_vertices = inserir_fim_vertice(lista_vertices, coord[0], coord[1], coord[2]);
+				tam_vertices++;
+			}
+			
+		}
+
+		separar = strtok(NULL, separador);
+	}	
+
+
+ 	glutInit(&argc,argv); // inicia a biblioteca glut. É a primeira rotina rotina a ser chamada
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500); // Tamanho da janela que abrirá
+	glutInitWindowPosition(0,0); // Posição em que a janela que abrirá irá aparecer na tela do PC
+	glutCreateWindow("Objeto 3D"); // Título da janela
+
+	init();
+	glutDisplayFunc(desenha_obj); // chama a função que construímos para desenhar
+	glutMainLoop(); // até esse comando ser chamado a janela não é exibida.
+
+	return 0;
+}
+
+
+// FUNÇÕES
+void desenha_obj()
+{
+  glViewport(0 , 0, 500, 500);
+
+  glClear(GL_COLOR_BUFFER_BIT); // pinta o buffer com a cor indicada para o funda da janela
+
+  // muda para o sistema de coordenadas do modelo
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity(); // inicializa a matriz de transformação atual
+  glTranslated(0, 0, -15); // define a posição do objeto na cena
+
+  // realiza operações de rotação no objeto
+  glRotated(30, 1, 0, 0);
+  glRotated(30, 0, 1, 0);
+  glRotated(30, 0, 0, 1);
+
+  Lados *acessa_face;
+  Vertices *acessa_vertice;
+
+  // Desenhando o objeto importado do blender
+  glColor3f(0,1,0);
+
+  // acessando a lista de faces e pegando a posição inicial
+  acessa_face = busca_face(0, lista_faces, &tam_faces);
+
+  // desenhando face por face
+  for (int i = 0; i < tam_faces; i++){
+  			
+	while (acessa_face != NULL){
+		glBegin(GL_LINE_LOOP);
+	
+		// Faces
+		for (int j = 0; j < tam_vert_por_face; j++){
+			acessa_vertice = busca_vertice(acessa_face->valor, lista_vertices, &tam_vertices);
+			acessa_face = acessa_face->prox;
+			glVertex3f(acessa_vertice->x, acessa_vertice->y, acessa_vertice->z);
+		}
+		glEnd();
+	}
+  }   
+
+  glFlush(); // esse comando exibe o que está armazenado no buffer
+         	 // se esse comando não for chamado, a janela abrirá mas não exibirá nada que foi feito
+         	 // e que está apenas no buffer.
+
+}
+
+void init(){
+  glClearColor(0.0, 0.0, 0.0, 0.0); // indica a cor que será usada no fundo da janela
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity(); // inicializa a matriz de projeção atual
+  glOrtho(-10.0, 10.0, -10.0, 10.0, 1, 20); 
+  //glFrustum(-5.0, 5.0, -5.0, 5.0, 1, 50);
+
+}
 
 // inserindo as faces no fim da lista
 Lados* inserir_fim_face(Lados *lista, int dado){
@@ -69,7 +271,7 @@ Vertices* inserir_fim_vertice(Vertices *lista, float x, float y, float z){
 }
 
 // imprimindo lista
-void imprimir_faces(int f, Lados *lista, int *tam_faces){
+Lados* busca_face(int f, Lados *lista, int *tam_faces){
 	Lados *percorre_lista = lista;
 
 	if (f >= *tam_faces){
@@ -82,12 +284,12 @@ void imprimir_faces(int f, Lados *lista, int *tam_faces){
 			percorre_lista = percorre_lista->prox;
 		}
 	}
-	
-	printf("%d\n", percorre_lista->valor);
+
+	return percorre_lista;
 	printf("\n");
 }
 
-void imprimir_vertices(int v, Vertices *lista, int *tam_vertices){
+Vertices* busca_vertice(int v, Vertices *lista, int *tam_vertices){
 	Vertices *percorre_lista = lista;
 
 	if (v >= *tam_vertices){
@@ -101,142 +303,7 @@ void imprimir_vertices(int v, Vertices *lista, int *tam_vertices){
 		}
 	}
 
-	printf("%f\n", percorre_lista->x);
-	printf("%f\n", percorre_lista->y);
-	printf("%f\n", percorre_lista->z);
-	// while(percorre_lista != NULL){
-	// 	printf("x = %f\n", percorre_lista->x);
-	// 	printf("y = %f\n", percorre_lista->y);
-	// 	printf("z = %f\n", percorre_lista->z);
-	// 	percorre_lista = percorre_lista->prox;
-	// }
+	return percorre_lista;
+
 	printf("\n");
-}
-
-// função para ler o arquivo
-void leitor(int *tam_faces, int *tam_vertices, int *tam_vert_por_face, Lados *lista_faces, Vertices *lista_vertices) {
-	FILE *arq;
-	char *ler;
-	char linha[100];
-	char str[10000] = "";
-	int i, num;
-
-	// inserindo elementos através do teclado. Condição de parada: "0" digitado.
-	/*while(1){
-		scanf("%d", &num);
-		if (num == 0)
-			break;
-		lista = inserir_fim_face(lista, num);
-	}*/
-
-	/*lista = inserir_fim_face(lista, 10);
-	lista = inserir_fim_face(lista, 20);
-	lista = inserir_fim_face(lista, 30);
-	lista = inserir_fim_face(lista, 40);
-	imprimir_faces(lista);*/
-
-	// lendo arquivo todo
-	arq = fopen("cubo.x3d", "r");
-	while(!feof(arq)){
-		ler = fgets(linha, 100, arq);
-		if (ler){
-			strcat(str, linha);
-		}
-		i++;
-	}
-	fclose(arq);
-	printf("\n");
-	
-	// Variáveis usadas para ler os vértices e faces desejadas
-	char *separar;
-	char separador[] = " \n";
-	int flag = 0;
-	int flag_coord = 1;
-	char face[4];
-	char vert[10];
-	float dado;
-	float coord[3];
-	bool flag_vert_face = false;
-
-	// Lendo só os vértices e faces desejadas
-	separar = strtok(str, separador);
-	while(separar != NULL){
-
-		if (separar[0] == 'c' && separar[1] == 'o' && separar[2] == 'o' && separar[3] == 'r' && separar[4] == 'd' &&\
-			separar[5] == 'I' && separar[6] == 'n' && separar[7] == 'd' && separar[8] == 'e' && separar[9] == 'x') {
-
-				flag = 1;
-				for (int i = 0; i < strlen(separar) - 1; i++){
-					if (separar[i] == '"'){
-						strcat(face, &separar[i + 1]);						
-						dado = atoi(face);				
-						lista_faces = inserir_fim_face(lista_faces, dado);
-						(*tam_vert_por_face)++;
-					}
-				}
-		}
-
-		else if (separar[0] == 'p' && separar[1] == 'o' && separar[2] == 'i' && separar[3] == 'n' && separar[4] == 't' &&\
-			separar[5] == '=' && separar[6] == '"') {
-
-				flag = 2;
-				for (int i = 0; i < strlen(separar) - 1; i++){
-					if (separar[i] == '"'){
-						strcat(vert, &separar[i + 1]);						
-						dado = atof(vert);
-
-						// setando o x do vértice
-						coord[0] = dado;
-						flag_coord++;
-					}
-				}
-		}
-
-		else if (strcmp(separar, "\"") == 0){
-			flag = 0;
-		}
-
-		else if (flag == 1){
-			dado = atoi(separar);		
-			lista_faces = inserir_fim_face(lista_faces, dado);
-			if (dado == -1){
-				(*tam_faces)++;
-				flag_vert_face = true;
-			}
-			if (flag_vert_face == false){
-				(*tam_vert_por_face)++;
-			}
-		}
-
-		else if (flag == 2){
-			dado = atof(separar);	
-
-			if (flag_coord == 1){
-				// setando o x do vértice
-				coord[0] = dado;
-				flag_coord++;
-			}
-
-			else if (flag_coord == 2){
-				// setando o y do vértice
-				coord[1] = dado;
-				flag_coord++;
-			}
-
-			else {
-				// setando o z do vértice
-				coord[2] = dado;
-				flag_coord = 1;
-				lista_vertices = inserir_fim_vertice(lista_vertices, coord[0], coord[1], coord[2]);
-				(*tam_vertices)++;
-			}
-
-			
-		}
-
-		separar = strtok(NULL, separador);
-	}	
-
-	// imprimir_faces(5, lista_faces, &tam_faces);
-	// imprimir_vertices(v, lista_vertices, &tam_vertices);
 }
